f1edd825f124a2c2ec06b3cca81b40009ed2a095
Pete Savage <psavage@redhat.com>
2019-04-27 11:31:00 +0100

    Added ms timer

--- ledled.ino
+++ ledled.ino
@@ -17,29 +17,45 @@ decode_results results;
 time_t start_time;
 float mod = 60.0f / NUM_LEDS;
 float mod12 = 12.0f / NUM_LEDS;
+float modpsec = 1000.0f / NUM_LEDS;
 
 float real_sec;
 float real_min;
+float real_psec;
+
+//Subsecond
+int psec_led;
+int psec_led2;
+float psec_bright;
+float psec_pre_led;
+int psec_led_old = 0;
+int psec_led2_old = 0;
 
 //Seconds
 int sec_led;
 int sec_led2;
 float sec_bright;
 float sec_pre_led;
+int sec_led_old = 0;
+int sec_led2_old = 0;
 
 //Minutes
 int min_led;
 int min_led2;
 float min_bright;
 float min_pre_led;
+int min_led_old = 0;
+int min_led2_old = 0;
 
 //Hours
 int hour_led;
 int hour_led2;
 float hour_bright;
 float hour_pre_led;
+int hour_led_old = 0;
+int hour_led2_old = 0;
 
-int bright_scale = 1;
+int bright_scale = 5;
 
 unsigned long cur_mil;
 float mills;
@@ -54,9 +70,32 @@ void setup() {
 }
 
 void loop() {
+  leds[psec_led_old] = CRGB(0, 0, 0);
+  leds[psec_led2_old] = CRGB(0, 0, 0);
+  leds[sec_led_old] = CRGB(0, 0, 0);
+  leds[sec_led2_old] = CRGB(0, 0, 0);
+  leds[min_led_old] = CRGB(0, 0, 0);
+  leds[min_led2_old] = CRGB(0, 0, 0);
+  leds[hour_led_old] = CRGB(0, 0, 0);
+  leds[hour_led2_old] = CRGB(0, 0, 0);
+  
   cur_mil = millis();
   mills = float(cur_mil % 1000) / 1000.0f;
   mills_and_time = cur_mil / 1000.0f;
+
+  //SubSeconds
+  real_psec = mills / 1000.0f;
+  psec_pre_led = mills * NUM_LEDS;
+  psec_led = (int)psec_pre_led;
+  psec_bright = psec_pre_led - psec_led;
+  psec_led2 = psec_led + 1 % (NUM_LEDS - 1);
+
+  leds[psec_led] = CRGB::Red;
+  leds[psec_led2] = CRGB::Red;
+  leds[psec_led2].nscale8(psec_bright * 256);
+  leds[psec_led].nscale8((1.0f - psec_bright) * 256);
+  leds[psec_led] /= bright_scale;
+  leds[psec_led2] /= bright_scale;
 
   //Seconds
   real_sec = ((float)second(start_time + mills_and_time) + mills) / 60.0f;
@@ -110,16 +149,22 @@ void loop() {
   //  leds[int((float)i / 4.0 * NUM_LEDS)] /= 60;
   //}
 
+  //if (irrecv.decode(&results)) {
+  //  Serial.println(results.value, HEX);
+  //  irrecv.resume(); // Receive the next value
+  //}
+  psec_led_old = psec_led;
+  psec_led2_old = psec_led2;
+  sec_led_old = sec_led;
+  sec_led2_old = sec_led2;
+  min_led_old = min_led;
+  min_led2_old = min_led2;
+  hour_led_old = hour_led;
+  hour_led2_old = hour_led2;
+
   FastLED.show();
-  if (irrecv.decode(&results)) {
-    Serial.println(results.value, HEX);
-    irrecv.resume(); // Receive the next value
-  }
-  delay(20);  
-  leds[sec_led] = CRGB(0, 0, 0);
-  leds[sec_led2] = CRGB(0, 0, 0);
-  leds[min_led] = CRGB(0, 0, 0);
-  leds[min_led2] = CRGB(0, 0, 0);
+  
+  //delay(20);  
 
   //Serial.println((float)minute(start_time + mills_and_time));
   //Serial.println((float)second(start_time + mills_and_time));
